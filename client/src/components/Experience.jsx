79050b9caeceb72812701c72e6401677c569d23e
ArmandDJsan <sc.armand@gmail.com>
2024-01-19 07:48:38 -0400

    loading multiplayer game 3d example

---
+++ client/src/components/Experience.jsx
@@ -0,0 +1,41 @@
+import { ContactShadows, Environment, OrbitControls, useCursor } from "@react-three/drei";
+import { AnimatedWoman } from './AnimatedWoman'
+import { useAtom } from "jotai";
+import { charactersAtom, socket } from "./SocketManager";
+import { useState } from "react";
+import * as THREE from "three";
+
+export const Experience = () => {
+  const [characters] = useAtom(charactersAtom);
+  const [onFloor, setOnFloor ] = useState(false);
+  useCursor(onFloor);
+
+  return (
+    <>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.5} />
+      <ContactShadows blur={2} />
+      <OrbitControls />
+      <mesh
+        rotation-x={-Math.PI / 2}
+        position={-0.001}
+        onClick={(e) => socket.emit("move", [e.point.x, 0, e.point.z]) }
+        onPointerEnter={(e) => setOnFloor(true)}
+        onPointerLeave={(e) => setOnFloor(false)}
+      >
+        <planeGeometry args={[10, 10]} />
+        <meshStandardMaterial color={"#f0f0f0"} />
+      </mesh>
+      {characters.map((char) => (
+        <AnimatedWoman
+          key={char.id}
+          position={new THREE.Vector3(char.position[0],char.position[1],char.position[2])}
+          topColor={char.topColor}
+          bottomColor={char.bottomColor}
+          hairColor={char.hairColor}
+          shoesColor={char.shoesColor} />
+      ))}
+    </>
+  );
+};
+/*  */
---
+++ server/index.js
@@ -0,0 +1,47 @@
+import { Server } from 'socket.io';
+import { Socket } from 'socket.io';
+
+const io = new Server({
+    cors: {
+        origin: "http://localhost:5173"
+    },
+})
+
+io.listen(3001);
+
+const characters = [];
+
+
+const newRandomPosition = () => {
+    return [Math.floor(Math.random() * 5), 0, Math.floor(Math.random() * 5)];
+}
+const newRandomColor = () => {
+    return ('#' + Math.round(Math.random() * 16777215 + 1).toString(16));
+}
+
+io.on("connection", (socket) => {
+
+    console.log("user connected");
+    socket.emit("connected", "bienvenido");
+    characters.push({
+        id: socket.id,
+        position: newRandomPosition(),
+        hairColor: newRandomColor(),
+        topColor: newRandomColor(),
+        bottomColor: newRandomColor(),
+        shoesColor: newRandomColor()
+    })
+    io.emit("characters", characters);
+    socket.on("disconnect", () => {
+        console.log("user disconnected...");
+        characters.splice(
+            characters.findIndex((character) => character.id === socket.id), 1
+        )
+        io.emit("characters", characters);
+    });
+    socket.on("move", (position) => {
+        const character = characters.find((character) => character.id === socket.id);
+        character.position = position;
+        io.emit("characters", characters);
+    }) 
+});
